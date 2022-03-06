cd0be8d1aa935d1cdce01fd7beaae3267d06cdec
shinpanda <duldugo1@gmail.com>
2022-03-06 22:20:35 +0900

    end lecture

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-// pixel modifier
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
 let painting = false;
+let filling = false;
+// pixel modifier
+function initCanvas() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  ctx.strokeStyle = INITIAL_COLOR;
+  ctx.fillStyle = INITIAL_COLOR;
+  ctx.lineWidth = 2.5;
+}
 
 function stopPainting() {
   painting = false;
@@ -21,7 +33,6 @@ function startPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
   if (!painting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
@@ -31,11 +42,68 @@ function onMouseMove(event) {
   }
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = ctx.strokeStyle;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick(event) {
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[🎨]";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting); //click
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
 
 /*
@@ -50,4 +118,6 @@ css 사이즈와 pixel manipulating 사이즈.
 아래와 같이 선언해주어야 작동한다.
 canvas.width = 700;
 canvas.height = 700;
+
+Array.from(object) -> object로부터 array를 만드는 함수
 */
